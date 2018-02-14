966db9dd9de87288653c52765e97bcc41062f7b1
MAS <digitalcolony@gmail.com>
2018-02-13 20:50:33 -0800

    weather icons, returning time from API - not server

--- index.js
+++ index.js
@@ -12,14 +12,18 @@ const lat = 47.60;
 const lng = -122.32;
 
 app.get("/", (req, res) => {
-    var weatherTime = new Date().timeNow(); // TODO: Add to weatherResults
+
     weather.getWeather(lat, lng,
         (errorMessage, weatherResults) => {
             if (errorMessage) {
                 res.status(400).send(errorMessage);
             } else {
-                res.status(200).send(`Seattle Temperature: <br>${Math.round(
-                    weatherResults.temperature )} F at ${weatherTime}`);
+                var weatherReport = `
+                    <h1>Yeti Weather</h1>
+                    <h2>Seattle: ${Math.round(weatherResults.temperature )} F</h2>
+                    <img src="${weatherResults.iconImage}" title="${weatherResults.icon}" alt="${weatherResults.icon}">
+                    <p><em>at ${timeConverter(weatherResults.time)}</em></p>`;
+                res.status(200).send(weatherReport);
             }
         });
 });
@@ -33,12 +37,23 @@ app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
 
-
 // For the time now
 Date.prototype.timeNow = function () {
     return ((this.getHours() < 10) ? "0" : "") + this.getHours() + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
 }
 
+function timeConverter(UNIX_timestamp) {
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes();
+    var sec = a.getSeconds() < 10 ? '0' + a.getSeconds() : a.getSeconds();
+    var time = month + ' ' + date + ', ' + year + ' ' + hour + ':' + min + ':' + sec;
+    return time;
+}
 module.exports = {
     app
 };
--- weather/weather.js
+++ weather/weather.js
@@ -8,6 +8,13 @@ var getWeather = (lat, lng, callback) => {
         (error, response, body) => {
             if (!error && response.statusCode === 200) {
                 callback(undefined, {
+                    time: body.currently.time,
+                    summary: body.currently.summary,
+                    icon: body.currently.icon,
+                    iconImage: getWeatherIconImage(body.currently.icon),
+                    precipIntensity: body.currently.precipIntensity,
+                    precipProbability: body.currently.precipProbability,
+                    humidity: body.currently.humidity,
                     temperature: body.currently.temperature,
                     apparentTemperature: body.currently.apparentTemperature
                 });
@@ -17,5 +24,28 @@ var getWeather = (lat, lng, callback) => {
         }
     );
 };
+
+function getWeatherIconImage(icon) {
+    var weatherIcons = {
+        "clear-day": "/images/256/Sunny.png",
+        "clear-night": "",
+        "rain": "/images/256/Rain.png",
+        "snow": "/images/256/snow.png",
+        "sleet": "/images/256/Showers.png",
+        "wind": "",
+        "fog": "/images/256/Fog.png",
+        "cloudy": "/images/256/Cloudy.png",
+        "partly-cloudy-day": "/images/256/Sunny-Interval.png",
+        "partly-cloudy-night": "",
+        "hail": "/images/256/Hail.png",
+        "thunderstorms": "/images/256/Thunderstorms.png",
+        "tornado": ""
+    };
+    var weatherIconImage = weatherIcons[icon];
+    if (weatherIconImage === "") {
+        weatherIconImage = "/images/256/Sunny.png";
+    }
+    return weatherIcons[icon];
+}
 
 module.exports.getWeather = getWeather;
