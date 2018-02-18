d9c9213dd4313a672cb6c92bc237a5a4d7fc06cc
MAS <digitalcolony@gmail.com>
2018-02-17 19:33:05 -0800

    Added Pug for templating

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
+const pug = require("pug");
 const weather = require("./weather/weather.js");
 
 var app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/'));
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
 
+app.use("/images/256", express.static(__dirname + "/images/256"));
 //hardcode Seattle for now
-const lat = 47.60;
+const lat = 47.6;
 const lng = -122.32;
 
 app.get("/", (req, res) => {
-
-    weather.getWeather(lat, lng,
-        (errorMessage, weatherResults) => {
-            if (errorMessage) {
-                res.status(400).send(errorMessage);
-            } else {
-                var weatherReport = `
-                    <h1>Yeti Weather</h1>
-                    <h2>Seattle: ${Math.round(weatherResults.temperature )} F</h2>
-                    <img src="${weatherResults.iconImage}" title="${weatherResults.icon}" alt="${weatherResults.icon}">
-                    <p><em>at ${weatherResults.momentTime}</em></p>`;
-                res.status(200).send(weatherReport);
-            }
-        });
+  weather.getWeather(lat, lng, (errorMessage, weatherResults) => {
+    if (errorMessage) {
+      res.status(400).send(errorMessage);
+    } else {
+      console.log("iconImage", weatherResults.iconImage);
+      res.render("index", {
+        temperature: `${Math.round(weatherResults.temperature)} F`,
+        iconImage: weatherResults.iconImage.toString(),
+        icon: weatherResults.icon,
+        forecastTime: weatherResults.momentTime
+      });
+    }
+  });
 });
 
 //The 404 Route (ALWAYS Keep this as the last route)
-app.get('*', function (req, res) {
-    res.status(404).send('404 Page Not Found');
+app.get("*", function(req, res) {
+  res.status(404).send("404 Page Not Found");
 });
 
 app.listen(port, () => {
-    console.log(`Started on port ${port}`);
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {
-    app
+  app
 };
--- weather/weather.js
+++ weather/weather.js
@@ -2,53 +2,60 @@ const request = require("request");
 const moment = require("moment-timezone");
 
 var getWeather = (lat, lng, callback) => {
-    request({
-            url: `https://api.darksky.net/forecast/${process.env.FORECAST_API_KEY}/${lat},${lng}`,
-            json: true
-        },
-        (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-                callback(undefined, {
-                    time: body.currently.time, // UNIX time in seconds
-                    timezone: body.timezone,
-                    momentTime: moment.tz((body.currently.time * 1000), body.timezone).format("YYYY-MM-DD HH:mm"),
-                    summary: body.currently.summary,
-                    icon: body.currently.icon,
-                    iconImage: getWeatherIconImage(body.currently.icon),
-                    precipIntensity: body.currently.precipIntensity,
-                    precipProbability: body.currently.precipProbability,
-                    humidity: body.currently.humidity,
-                    temperature: body.currently.temperature,
-                    apparentTemperature: body.currently.apparentTemperature
-                });
-            } else {
-                callback("Unable to fetch the weather");
-            }
-        }
-    );
+  request(
+    {
+      url: `https://api.darksky.net/forecast/${
+        process.env.FORECAST_API_KEY
+      }/${lat},${lng}`,
+      json: true
+    },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        callback(undefined, {
+          time: body.currently.time, // UNIX time in seconds
+          timezone: body.timezone,
+          momentTime: moment
+            .tz(body.currently.time * 1000, body.timezone)
+            .format("YYYY-MM-DD HH:mm"),
+          summary: body.currently.summary,
+          icon: body.currently.icon,
+          iconImage: getWeatherIconImage(body.currently.icon),
+          precipIntensity: body.currently.precipIntensity,
+          precipProbability: body.currently.precipProbability,
+          humidity: body.currently.humidity,
+          temperature: body.currently.temperature,
+          apparentTemperature: body.currently.apparentTemperature
+        });
+      } else {
+        callback("Unable to fetch the weather");
+      }
+    }
+  );
 };
 
+// TODO: get better and more complete set of weather icons
+
 function getWeatherIconImage(icon) {
-    var weatherIcons = {
-        "clear-day": "images/256/Sunny.png",
-        "clear-night": "",
-        "rain": "images/256/rain.png",
-        "snow": "images/256/snow.png",
-        "sleet": "images/256/Showers.png",
-        "wind": "",
-        "fog": "images/256/Fog.png",
-        "cloudy": "images/256/Cloudy.png",
-        "partly-cloudy-day": "images/256/Sunny-Interval.png",
-        "partly-cloudy-night": "",
-        "hail": "images/256/Hail.png",
-        "thunderstorms": "images/256/Thunderstorms.png",
-        "tornado": ""
-    };
-    var weatherIconImage = weatherIcons[icon];
-    if (weatherIconImage === "") {
-        weatherIconImage = "images/256/Sunny.png";
-    }
-    return weatherIcons[icon];
+  var weatherIcons = {
+    "clear-day": "images/256/Sunny.png",
+    "clear-night": "",
+    rain: "images/256/rain.png",
+    snow: "images/256/snow.png",
+    sleet: "images/256/Showers.png",
+    wind: "",
+    fog: "images/256/Fog.png",
+    cloudy: "images/256/Cloudy.png",
+    "partly-cloudy-day": "images/256/Sunny-Interval.png",
+    "partly-cloudy-night": "",
+    hail: "images/256/Hail.png",
+    thunderstorms: "images/256/Thunderstorms.png",
+    tornado: ""
+  };
+  var weatherIconImage = weatherIcons[icon];
+  if (weatherIconImage === "") {
+    weatherIconImage = "images/256/Sunny.png";
+  }
+  return weatherIconImage;
 }
 
 module.exports.getWeather = getWeather;
